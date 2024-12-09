879a1acad18a6fd9e4a047beb3176ac7e2fc5232
George Monibah <monibahgeorgew@gmail.com>
2024-12-09 14:57:52 +0000

    added basic_ops and Python_intro

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+add_result = number1 + number2
+minus_result = number1 - number2
+product_result = number1 * number2
+
+print(add_result)
+print(minus_result)
+print(product_result)
