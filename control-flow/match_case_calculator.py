b9da4045fafe194f2ae5f0e356a2b78ce00971dc
George Monibah <monibahgeorgew@gmail.com>
2024-12-18 16:31:07 +0000

    added a calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,7 +1,7 @@
 num1 = int(input("Enter the first number:"))
 num2 = int(input("Enter the second number:"))
 
-operation = str(input("Choose the operation(+,-,*,/"))
+operation = str(input("Choose the operation(+, -, *, /"):)
 
 match operation
 
