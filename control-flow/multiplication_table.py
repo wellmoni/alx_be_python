88ca308d49416456e2c01bc285d5fdfc6495ac97
George Monibah <monibahgeorgew@gmail.com>
2024-12-19 20:49:58 +0000

    Added a multiplaction table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,6 @@
 number = int(input("Enter a number to see its multiplication table: "))
 y = int(input("Please enter the range of the multiplication table: "))
-for x in range (1, y + 1):
+for x in range (1,  12):
     print(f"{number} x {x} = number * x}" )
 
 
