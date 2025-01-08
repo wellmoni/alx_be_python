ef4e261cfee16396a11a26bb6f64fc4a03191067
George Monibah <monibahgeorgew@gmail.com>
2025-01-08 22:55:16 +0000

    adding a function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,23 @@
+def perform_operation(num1, num2,operation):
+    
+    """
+    this is going to perform a basic arithmetic operation: addition, subtraction, multiplication or division.
+    Parameter:
+        num1 (float): First number.
+        num2 (floa): second number.
+        operation (str): This operation to perform ('add', 'substract', 'multiply', 'divide')
+    Returns:
+        Float or srt:
+    """
+    if operation == "add":
+        return num1 + num2
+    elif operation == "substract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif opeartion == "divide":
+        if num2 ==0:
+            return "Error: Zero is not allowed."
+        return num1 / num2
+    else:
+        return "Error: Invalid Operation."
