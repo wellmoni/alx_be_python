9610637be45ade4ad7c3e4c49e51f97ebed9aa19
wellmoni <wellmoni95@gmail.com>
2025-01-16 10:12:59 +0000

    Added Bank Account

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,24 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+
+        if amount>0:
+            self.__account_balance = account_balance + amount
+        else:
+            raise ValueError("Deposit amount must be positive.")
+    def withdraw(self, amount):
+
+        if amount <=0:
+            raise ValueError("Withdrawal amount must be positive.")
+        if amount > self.__account_balance:
+            return False
+        self.__account_balance = account_balance-amount
+            return True
+
+        def display_balance(self):
+
+            print(f"Current balance: ${self.__account_balance:.2f}")
+
+
