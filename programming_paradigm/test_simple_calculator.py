a37fd89370dd596b91779aa09ad1bb53e8c86bd2
wellmoni <wellmoni95@gmail.com>
2025-01-17 22:00:04 +0000

    Added a test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,11 +7,11 @@ class TestSimpleCalculator(unittest.TestCase):
         self.calculator =SimpleCalculator()
 
     def test_addition(self):
-        self.assertEqual(self.calculator.add(2,3),5)
+        self.assertEqual(self.calc.add(2,3),5)
     def test_subtract(self):
-        self.assertEqual(self.calculator.subtract(5,3)2)
+        self.assertEqual(self.calc.subtract(5,3)2)
     def test_multiply(self):
-        self.assertEqual(self.calculator.multiply(4,4)16)
+        self.assertEqual(self.calc.multiply(4,4)16)
     def test_divide(self):
-        self.assertEqual(self.calculator.divide(4,1)4)
+        self.assertEqual(self.calc.divide(4,1)4)
 
