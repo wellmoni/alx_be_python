dd2ab03ef8356669b7ebbb26fb9693b5a55ab182
George Monibah <monibahgeorgew@gmail.com>
2025-01-24 13:15:01 +0000

    Added a library System

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,8 +1,8 @@
 class Book:
     def __init__(self, title, author):
 
-        self.title
-        self.author
+        self.title = title
+        self.author = author
 
     def __str__(self):
         return f"'{self.title}' by {self.author}"
