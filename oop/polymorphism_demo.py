be3f62a49354387243fac128680689d8e5f74376
wellmoni <wellmoni95@gmail.com>
2025-01-26 21:39:32 +0000

    added poly

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,19 @@
+import math
+
+class Shape:
+    def area(self):
+        raise NotImpletedError("Derived classes must override this method")
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length =length
+        self.width = width
+    def area(self):
+        return self.length * self.width
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return math.pi * (self.radius *2)
+    def print_area(shape):
+        print(f" The area of the {shape.__class__.__name__.lower()} is: {shape.area()}")
+
