d975adfccbea514ce0b509b8d5e39cc4d9a152ff
wellmoni <wellmoni95@gmail.com>
2025-01-26 21:49:34 +0000

    added class method

---
+++ oop/class_static_method.py
@@ -0,0 +1,15 @@
+class Calculator:
+    calculation_type = "Arithmetic Operations"
+
+    @staticmethod
+
+    def add(a, b):
+
+        return a + b
+    @classmethod
+    def multiply(cls, a, b):
+
+        print(f"Performing:{cls.calculation_type}")
+
+        return a * b
+
