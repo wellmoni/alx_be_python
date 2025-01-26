34ec401159ddb9e91e7b7fb558763c54f714dfaf
wellmoni <wellmoni95@gmail.com>
2025-01-26 21:53:24 +0000

    added class method

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -9,7 +9,7 @@ class Calculator:
     @classmethod
     def multiply(cls, a, b):
 
-        print(f"Performing:{cls.calculation_type}")
+        print(f"Calculation type:{cls.calculation_type}")
 
         return a * b
 
